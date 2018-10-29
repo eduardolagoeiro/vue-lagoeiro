40d2a545da48fbde48b9f1638453bf9be7244122
Eduardo Lagoeiro <eduardolagoeiro@id.uff.br>
2018-10-29 17:32:01 -0300

    mobile menu

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
   <v-app>
     <v-toolbar app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer">
+      </v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
         <span class="font-weight-light">Família Lagoeiro</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn flat to="/">Home</v-btn>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat :to="{path:'/', name:'home'}">Home</v-btn>
         <v-btn flat :to="{path:'/people', name:'people'}">Pessoas</v-btn>
-        <v-btn flat>História</v-btn>
-        <v-btn flat>Curiosidades</v-btn>
+        <v-btn flat :to="{path:'/history', name:'history'}">História</v-btn>
+        <v-btn flat :to="{path:'/curiosity', name:'curiosity'}">Curiosidades</v-btn>
       </v-toolbar-items>
     </v-toolbar>
-
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+      fixed
+      width = "300"
+      id = "drawer"
+    >
+      <v-layout column align-center wrap justify-center>
+        <v-btn class="v-btn-100" flat :to="{path:'/', name:'home'}">Home</v-btn>
+        <v-btn class="v-btn-100" flat :to="{path:'/people', name:'people'}">Pessoas</v-btn>
+        <v-btn class="v-btn-100" flat :to="{path:'/history', name:'history'}">História</v-btn>
+        <v-btn class="v-btn-100" flat :to="{path:'/curiosity', name:'curiosity'}">Curiosidades</v-btn>
+      </v-layout>
+    </v-navigation-drawer>
     <v-content>
       <router-view/>
     </v-content>
@@ -24,8 +39,17 @@ export default {
   name: 'App',
   data () {
     return {
-      defaultId: 1
+      defaultId: 1,
+      drawer: null
     }
   }
 }
 </script>
+
+<style>
+  .v-btn-100 {
+    width: 100%;
+    padding: 2.5em 0;
+    margin: 0;
+  }
+</style>
