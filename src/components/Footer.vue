0f3225d9aaaf8c68a513d886c04ff3ae2d35d012
Eduardo Lagoeiro <eduardolagoeiro@id.uff.br>
2018-11-01 10:56:36 -0300

    footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-footer dark height="auto">
+    <v-layout row wrap pb-2 justify-center align-center>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn icon :ripple="false" href="https://github.com/eduardolagoeiro" target="_blank">
+          <v-icon medium>fa-github</v-icon>
+        </v-btn>
+        <v-btn icon :ripple="false" href="https://br.linkedin.com/in/eduardo-lagoeiro-b1b027137" target="_blank">
+          <v-icon medium>fa-linkedin</v-icon>
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </v-footer>
+</template>
